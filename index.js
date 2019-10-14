ea3d76c1e488270454cfabc161772390820bc0fe
Node Code Formatter <formatter@1337z.ninja>
2019-10-14 16:02:16 +0000

    Formatted code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getWeekNumber (d) {
   const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1))
   // Calculate full weeks to nearest Thursday
   const weekNum = Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
-  console.info("Hello")
+  console.info('Hello')
   // Return week number
   return weekNum
 }
